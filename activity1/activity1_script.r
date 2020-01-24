d840e559570d2df7ec348fd3bf5c033032d6051b
Abiy Tekle '20 <atekle@colgate.edu>
2020-01-24 12:12:17 -0500

    calculations and creating vectors

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,2 +1,19 @@
 # Activity 1
 print("hello R world")
+# 6 to the power of 6
+6^6 
+#add 5 and 210
+5+210
+#3 minus 10
+3-10
+#assigning an integer to a variable named a
+a<-2446
+#multiply the number assigned by 5
+5*a
+#divide the number assigned by 2
+a/2
+#create a vector of 3 numbers and assign it to a varible named b
+b<-c(2446,40,10000,3)
+#divide all numbers by 2
+b/2
+
