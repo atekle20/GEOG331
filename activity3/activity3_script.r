5c1c6cfc09d118ba853b4c167702df91a5b672bc
atekle20 <39914665+atekle20@users.noreply.github.com>
2020-02-25 18:45:53 -0500

    final code

--- activity3/activity3_script.r
+++ activity3/activity3_script.r
@@ -66,6 +66,8 @@ datW[datW$air.tempQ1 < 8,]
 #look at days with really high air temperature
 datW[datW$air.tempQ1 > 33,]  
 
+
+#Question 5
 #plot precipitation and lightning strikes on the same plot
 #normalize lighting strikes to match precipitation
 lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
@@ -81,3 +83,73 @@ points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 
 #plot lightning points only when there is lightning     
 points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
        col= "tomato3", pch=19)
+
+#create a lightscale column in datW
+datW$lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+
+assert <- function(statement,err.message){
+  #if evaluates if a statement is true or false for a single item
+  if(statement == FALSE){
+    print(err.message)
+  }
+  
+}
+#check if the values are equal and return error if they are not equal 
+assert(isTRUE(all.equal(datW$DD[lightscale > 0],datW$DD[datW$lightscale > 0])),"They are not equal")
+
+
+#Question 6
+#the first argument is a logical statement to be evaluated as true or false on a vector
+#the second argument is the value that my air.tempQ1 column will be given if the statement
+#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
+#In this case it is just given the air temperature value
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
+#create a new air temp column
+datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.
+#create a new wind speed column
+datW$wind.speed2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$wind.speed ))
+#wind.speed2 NA values  cannot be greater than wind.speed NA values
+assert(length(which(is.na(datW$wind.speed)))<=length(which(is.na(datW$wind.speed2))),"The wind speed has more NA values.")
+
+plot(datW$DD,datW$wind.speed2,xlab="Day of year",ylab = "wind speed in ms",type="b")
+
+#Question 7
+#make a plot with filled in points (using pch)
+#line lines
+#look at the soil temperature and moisture data before day 193
+plot(datW$DD[datW$DD>189 & datW$DD<193], datW$soil.moisture[datW$DD>189 & datW$DD<193], pch=19, type="b", xlab = "Day of Year",
+     ylab="soil moisture in cm3 water per cm3 soil")
+
+plot(datW$DD[datW$DD>189 & datW$DD<193], 
+     datW$soil.temp[datW$DD>189 & datW$DD<193], pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil temperature (degrees C)")
+
+#check the difference between the soil temperature and air temperature
+check.difference<-datW$soil.temp[datW$DD>189 & datW$DD<193]-datW$air.temperature[datW$DD>189 & datW$DD<193]
+plot(datW$DD[datW$DD>189 & datW$DD<193], 
+     check.difference, pch=19, type="b", xlab = "Day of Year",
+     ylab="Difference of soil and air temperature (degrees C)")
+#Question 8
+Avg.air.temp<-mean(datW$air.temperature)
+Avg.wind.speed<-mean(datW$wind.speed)
+Avg.soil.moisture<-mean(datW$soil.moisture,na.rm=TRUE)
+Avg.soil.temp<-mean(datW$soil.temp,na.rm=TRUE)
+total.precipitation<-sum(datW$precipitation)
+len.air.temp<-length(datW$air.temperature)
+len.wind.speed<-length(datW$wind.speed)
+len.soil.moisture<-length(which(!is.na(datW$soil.moisture)))
+len.soil.temp<-length(which(!is.na(datW$soil.temp)))
+len.total.precipitation<-length(datW$precipitation)
+starting.date<-min(datW$doy)
+End.date<-max(datW$doy)
+#Question 9
+plot(datW$DD,datW$soil.moisture,xlab="Day of year",ylab = "soil moisture in cm3 water per cm3 soil",type="b")
+plot(datW$DD,datW$air.temperature,xlab="Day of year",ylab = "air temp in degree c",type="b")
+plot(datW$DD,datW$soil.temp,xlab="Day of year",ylab = "soil temp in degree c",type="b")
+plot(datW$DD,datW$precipitation,xlab="Day of year",ylab = "precipitation in mm",type="b")
+
