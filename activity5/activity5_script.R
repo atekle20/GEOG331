c501005811e625000bdbb8413cb36591090a8c98
atekle20 <39914665+atekle20@users.noreply.github.com>
2020-03-04 10:21:18 -0500

    question 5 to 9

--- activity5/activity5_script.R
+++ activity5/activity5_script.R
@@ -1,12 +1,12 @@
 #load in lubridate
 library(lubridate)
 #read in streamflow data
-datH <- read.csv("y:\\Data\\activities\\a05\\stream_flow_data.csv",
+datH <- read.csv("Y:\\Students\\atekle\\a05\\stream_flow_data.csv",
                  na.strings = c("Eqp"))
 head(datH)    
 #read in precipitation data
 #hourly precipitation is in mm
-datP <- read.csv("y:\\Data\\activities\\a05\\2049867.csv")                          
+datP <- read.csv("Y:\\Students\\atekle\\a05\\2049867.csv")                          
 head(datP)
 
 #only use most reliable measurements
@@ -36,18 +36,22 @@ datD$hour <- hour(timesD ) + (minute(timesD )/60)
 #get full decimal time
 datD$decDay <- datD$doy + (datD$hour/24)
 #calculate a decimal year, but account for leap year
-datD$decYear <- ifelse(leap_year(datD$year),datD$year + (datD$decDay/367),
-                       datD$year + (datD$decDay/365))
+datD$decYear <- ifelse(leap_year(datD$year),datD$year + ((datD$decDay-1)/366),
+                       datD$year + ((datD$decDay-1)/365))
 #calculate times for datP                       
 datP$hour <- hour(dateP ) + (minute(dateP )/60)
 #get full decimal time
 datP$decDay <- datP$doy + (datP$hour/24)
 #calculate a decimal year, but account for leap year
-datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/367),
-                       datP$year + (datP$decDay/365))  
+datP$decYear <- ifelse(leap_year(datP$year),datP$year + ((datP$decDay-1)/366),
+                       datP$year + ((datP$decDay-1)/365))  
 
+#Question 4
 #plot discharge
+par(mar=c(5,4,4,2)+1)
 plot(datD$decYear, datD$discharge, type="l", xlab="Year", ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+
+
 
 #basic formatting
 aveF <- aggregate(datD$discharge, by=list(datD$doy), FUN="mean")
@@ -159,3 +163,173 @@ legend("topright", c("mean","1 standard deviation"), #legend items
        col=c("black",rgb(0.392, 0.584, 0.929,.2)),#colors
        pch=c(NA,15),#symbols
        bty="n")#no legend border
+
+#Quetion 5
+
+
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,90),
+     xaxs="i", yaxs ="i",#remove gaps from axes
+     axes=FALSE)#no axes
+
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.392, 0.584, 0.929,.2), #color that is semi-transparent
+        border=NA#no border
+)       
+
+lines(datD$doy[datD$year==2017],datD$discharge[datD$year==2017], col="yellow")
+
+months<-c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+month<-c(31,31+28,31+31+28,31+31+28+30,31+31+28+30+31,31+31+28+30+31+30,31+31+28+30+31+30+31,31+31+28+30+31+30+31+31,
+         31+31+28+30+31+30+31+31+30,31+31+28+30+31+30+31+31+30+31,31+31+28+30+31+30+31+31+30+31+30,
+         31+31+28+30+31+30+31+31+30+31+30+31)
+#month<-month-30
+axis(side=1,at=month,label=months) #tick labels
+axis(2, seq(0,80, by=20),
+     seq(0,80, by=20),
+     las = 2)#show ticks at 90 degree angle'
+legend("topright", c("mean","1 standard deviation","2017"), #legend items
+       lwd=c(2,NA,1),#lines
+       col=c("black",rgb(0.392, 0.584, 0.929,.2),"yellow"),#colors
+       pch=c(NA,15,NA),#symbols
+       bty="n")#no legend border
+
+#Question 7
+library(dplyr)
+#groupy doy and year
+group_by(datP,datP$doy,datP$year)
+#create a data frame that has column with a count of entries in the 2 data frames
+datP.count<-count(datP,datP$doy,datP$year)
+datP.count$all24<-ifelse(datP.count$n ==24, 1, 0)
+
+names(datP.count)[1]<-"doy"
+names(datP.count)[2]<-"year"
+#create a new data frame that joins the 2 dataframes based on doy and year
+datDnew<-right_join(datD,datP.count,by=c("doy","year"))
+#omit all na values
+na.rm(datDnew)
+
+# plot(datD$doy,datD$discharge, col=ifelse(datD$doy[datP.count$all24 ==1],"black","red"))
+# #plot points when all precipitation measurements are available     
+# points(datD$doy[datP.count$all24 ==1], lightscale[lightscale > 0],
+#         col= "tomato3", pch=19)
+
+plot(datDnew$doy[datDnew$year==2009],datDnew$discharge[datDnew$year==2009], col="black")
+#plot points when all precipitation measurements are available   
+
+points(datDnew$doy[which(datDnew$year==2009 & datDnew$all24 ==1),],datDnew$discharge[datDnew$year==2009,datDnew$all24 ==1],
+       col= "tomato3", pch=19)
+
+#Question 8
+#example
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 248 & datD$doy < 250 & datD$year == 2011,]
+hydroP <- datP[datP$doy >= 248 & datP$doy < 250 & datP$year == 2011,]
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#celing rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+        polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+                  hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+                c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+                col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+####
+#Question 8
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 12 & datD$doy < 14 & datD$year == 2011,]
+hydroP <- datP[datP$doy >= 12 & datP$doy < 14 & datP$year == 2011,]
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#celing rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+        polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+                  hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+                c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+                col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+
+#Question 9
+#get month of year
+datD$month<-month(datesD)
+#convert it into a factor
+datD$month<-as.factor(datD$month)
+#changed the name of each factor
+levels(datD$month)<-list(Jan="1",Feb="2",Mar="3",Apr="4",May="5",Jun="6",Jul="7",Aug="8",Sep="9",Oct="10",Nov="11",Dec="12")
+
+season <- datD$season
+#divide up your seasons by month
+winter <-c("Dec","Jan", "Feb")
+spring <- c("Apr", "May","Mar")
+summer <- c("Jun", "Jul", "Aug")
+fall <- c("Sep","Oct", "Nov")
+
+
+#reformat to work with grep
+winter <- paste(winter,collapse="|")
+spring <- paste(spring,collapse="|")
+summer<- paste(summer,collapse="|")
+spring <- paste(spring,collapse="|")
+fall <- paste(fall,collapse="|")
+
+#populate the season field based on matching seasons (above) in the month field
+season[grep(winter, datD$month)] <- "winter"
+season[grep(spring, datD$month)] <- "spring"
+season[grep(summer, datD$month)] <- "summer"
+season[grep(fall, datD$month)] <- "fall"
+
+library(ggplot2)
+#make a violin plot
+ggplot(data= subset(datD,year==2016), aes(season,discharge)) + 
+        geom_violin()+ggtitle("2016")
+ggplot(data= subset(datD,year==2017), aes(season,discharge)) + 
+        geom_violin()+ggtitle("2017")
